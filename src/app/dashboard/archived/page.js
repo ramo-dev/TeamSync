7a6f697e87e3aca4b234c21a470c61afe41d3536
ramo-dev <annuar.dev@gmail.com>
2024-07-23 20:52:58 +0300

    some dang bug, hopefully...,again

---
+++ src/app/dashboard/archived/page.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+
+const Page = () => {
+  return <h1>Hello world: archived component</h1>
+}
+
+export default Page;
---
+++ src/app/dashboard/settings/page.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+
+const Page = () => {
+  return <h1>Hello world: Settings component</h1>
+}
+
+export default Page;
